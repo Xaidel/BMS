82dd4dc86b573a8e86ab18a530b4c848fe7ade2b
jptaycs <125124706+jptaycs@users.noreply.github.com>
2025-07-16 17:05:06 +0800

    Add summary card for resident, blotter, cert, house and event

---
+++ src/components/ui/summarycardhousehold.tsx
@@ -0,0 +1,21 @@
+// components/ui/summarycardHousehold.tsx
+
+type SummaryCardHouseholdProps = {
+  title: string;
+  value: number | string;
+  icon: JSX.Element;
+};
+
+export default function SummaryCardHousehold({ title, value, icon }: SummaryCardHouseholdProps) {
+  return (
+    <div className="flex justify-between items-center p-3 bg-white shadow-md rounded-lg w-[270px] h-[100px]">
+      <div>
+        <p className="text-sm text-gray-500">{title}</p>
+        <p className="text-xl font-bold">{value}</p>
+      </div>
+      <div className="text-3xl text-gray-400">
+        {icon}
+      </div>
+    </div>
+  );
+}

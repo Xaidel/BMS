be15d132e3b065e62d73024a13194487ae0dc572
Xaidel <abechuelak@gmail.com>
2025-07-22 19:49:44 +0800

    feat: blotter report download

--- src/components/pdf/Stylesheet.tsx
+++ src/components/pdf/Stylesheet.tsx
@@ -9,5 +9,29 @@ export const styles = StyleSheet.create({
     display: "flex",
     justifyContent: "center",
     alignItems: "center"
+  },
+  table: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  headerCell: {
+    border: "1px solid black",
+    padding: 5,
+    width: "12.5%",
+    fontSize: 10,
+    fontWeight: 700,
+    backgroundColor: "#e0e0e0", // Light gray for contrast
+    textAlign: "center",
+  },
+  tableRow: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  tableCell: {
+    border: "1px solid black",
+    padding: "5px",
+    width: "12.5%",
+    fontSize: 10,
   }
+
 })
